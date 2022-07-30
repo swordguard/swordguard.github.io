dc24da1792f7dbe7bfb404e21a25ed9f005a0f1c
Jian <jay.sun71503@gmail.com>
2022-07-30 21:02:57 +1000

    Updates

--- src/constants/constants.ts
+++
@@ -1,3 +0,0 @@
-// export const SERVER_ADDRESS = 'http://localhost:3001'
-export const SERVER_ADDRESS = 'https://ms-website-api.herokuapp.com'
-export const FEEDBACK_ENDPOINT = `${SERVER_ADDRESS}/feedback`
